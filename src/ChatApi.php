84820ba5c4e215c45cfbe6d73d2ee4631ad4aedf
Jorge Gonçalves Junior <jorge.techinfo@gmail.com>
2018-08-10 22:15:11 -0300

    api functions

--- src/ChatApi.php
+++ src/ChatApi.php
@@ -11,12 +11,24 @@ namespace ChatApi;
 
 class ChatApi
 {
+    /**
+     * @var string
+     */
     protected $endPoint;
 
+    /**
+     * @var string
+     */
     protected $token;
 
+    /**
+     * @var \GuzzleHttp\Client
+     */
     protected $client;
 
+    /**
+     * ChatApi constructor.
+     */
     public function __construct()
     {
         $this->endPoint = config('chat_api.endpoint');
@@ -25,45 +37,82 @@ class ChatApi
         $this->client = new \GuzzleHttp\Client(['verify' => true]);
     }
 
-    public function sendMessage($destination, $message, array $options = [])
+    /**
+     * @return string
+     */
+    public function getStatus()
     {
+        $response = $this->client->request('GET', $this->endPoint . '/status');
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @return string
+     */
+    public function getQRCode()
+    {
+        $response = $this->client->request('GET', $this->endPoint . '/qr_code');
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @param $groupName
+     * @param array $destinations
+     * @param $message
+     * @return string
+     */
+    public function sendGroupMessage($groupName, array $destinations, $message)
+    {
+        $data = $this->buildFormRequest([
+            'groupName' => $groupName,
+            'messageText' => $message,
+            'phones' => $destinations
+        ]);
+
+        $response = $this->client->request('POST', $this->endPoint . '/group', $data);
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @param $destination
+     * @param $message
+     * @return string
+     */
+    public function sendMessage($destination, $message)
+    {
+        /*
+         * chatId parameter its not been used for now
+         * See docs: https://app.chat-api.com/docs
+         */
         $data = $this->buildFormRequest([
             'body' => $message,
             'phone' => $destination
         ]);
 
-        $response = $this->client->request('POST', $this->endPoint . '/message', $data);
+        $response = $this->client->request('POST', $this->endPoint . '/sendMessage', $data);
 
         return $response->getBody()->getContents();
 
     }
 
-    public function setWebhook($url)
+    /**
+     * @param $destination
+     * @param $content
+     * @param $filename
+     * @return string
+     */
+    public function sendFile($destination, $content, $filename)
     {
         $data = $this->buildFormRequest([
-            'webhookUrl' => $url
+            'body' => $content,
+            'phone' => $destination,
+            'filename' => $filename
         ]);
 
-        $this->client->request('POST', $this->endPoint . '/webhook', $data);
-    }
-
-    public function getWebhook()
-    {
-        $response = $this->client->request('GET', $this->endPoint . '/webhook');
-
-        return $response->getBody()->getContents();
-    }
-
-    public function logout()
-    {
-        $response = $this->client->request('GET', $this->endPoint . '/logout');
-
-        return $response->getBody()->getContents();
-    }
-
-    public function reboot()
-    {
-        $response = $this->client->request('GET', $this->endPoint . '/reboot');
+        $response = $this->client->request('POST', $this->endPoint . '/sendFile', $data);
 
         return $response->getBody()->getContents();
     }
@@ -74,11 +123,73 @@ class ChatApi
      */
     public function getMessages($lastMessageNumber = null)
     {
-        $query = $lastMessageNumber ?
+        $query = $lastMessageNumber >= 0 ?
             "?token={$this->token}&lastMessageNumber={$lastMessageNumber}" :
             "?token={$this->token}&last";
 
         $response = $this->client->request('GET', $this->endPoint . '/messages' . $query);
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @param $url
+     */
+    public function setWebhook($url)
+    {
+        $data = $this->buildFormRequest([
+            'webhookUrl' => $url
+        ]);
+
+        $this->client->request('POST', $this->endPoint . '/webhook', $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebhook()
+    {
+        $response = $this->client->request('GET', $this->endPoint . '/webhook');
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @return string
+     */
+    public function logout()
+    {
+        $response = $this->client->request('GET', $this->endPoint . '/logout');
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @return string
+     */
+    public function reboot()
+    {
+        $response = $this->client->request('GET', $this->endPoint . '/reboot');
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessagesQueue()
+    {
+        $response = $this->client->request('GET', $this->endPoint . '/showMessagesQueue');
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @return string
+     */
+    public function clearMessagesQueue()
+    {
+        $response = $this->client->request('GET', $this->endPoint . '/clearMessagesQueue');
 
         return $response->getBody()->getContents();
     }
